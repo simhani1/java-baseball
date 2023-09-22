27e7373f8d61373835d6172bf9d5b2e5c8c0bbdd
simhani1 <simhani1@gmail.com>
2023-09-23 02:07:33 +0900

    test: 4글자 이상의 문자열 입력 테스트

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -21,6 +21,18 @@ class GameControllerTest extends NsTest {
         );
     }
 
+    @Test
+    void 입력한_문자열의_길이가_4이상이면_예외를_던진다() {
+        // given
+        String inputNumber = "1234";
+
+        // then
+        assertSimpleTest(
+                () -> Assertions.assertThatThrownBy(
+                        () -> runException(inputNumber)).isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
