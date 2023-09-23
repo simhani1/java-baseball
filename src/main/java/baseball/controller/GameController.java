b248d1e351bd4b698fdc6939458b62ac58565e17
simhani1 <simhani1@gmail.com>
2023-09-23 20:54:48 +0900

    feat: controller 로직 재구성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -2,14 +2,16 @@ package baseball.controller;
 
 import baseball.controller.message.Message;
 import baseball.controller.validator.Validator;
+import baseball.domain.Computer;
 import baseball.domain.GameResult;
 import baseball.service.CheckAnswerService;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class GameController {
-
     private CheckAnswerService checkAnswerService;
+
+    private static final int RETRY_COMMAND = 1;
 
     public GameController(CheckAnswerService checkAnswerService) {
         this.checkAnswerService = checkAnswerService;
@@ -17,20 +19,55 @@ public class GameController {
 
     public void run() {
         print(Message.STARTING_MESSAGE);
-        while (true) {
-            print(Message.INPUT_NUMBER);
-            GameResult gameResult = checkAnswerService.isAnswer(checkFormat(readLine()));
+        do {
+            Computer computer = new Computer();
+            GameResult gameResult;
+            do {
+                print(Message.INPUT_NUMBER);
+                gameResult = checkAnswerService.checkAnswer(computer, checkNumberFormat(readLine()));
+                print(gameResult);
+            } while (!gameResult.isCorrect());
+        } while (retryGame());
+    }
+
+    private String checkNumberFormat(String inputNumber) {
+        if (Validator.checkNumberFormat(inputNumber)) {
+            return inputNumber;
         }
+        throw new IllegalArgumentException();
     }
 
     private void print(Message message) {
         System.out.println(message.getMessage());
     }
 
-    private String checkFormat(String inputNumber) {
-        if (Validator.isThreeLetters(inputNumber) && Validator.isComposedOfNumber(inputNumber)) {
-            return inputNumber;
+    private void print(GameResult gameResult) {
+        String message = "";
+        if (gameResult.getBallCount() >= 1) {
+            message += (gameResult.getBallCount() + "볼 ");
+        }
+        if (gameResult.getStrikeCount() >= 1) {
+            message += (gameResult.getStrikeCount() + "스트라이크");
+        }
+        if (gameResult.getBallCount() == 0 && gameResult.getStrikeCount() == 0) {
+            message += "낫싱";
+        }
+        System.out.println(message);
+    }
+
+    private boolean retryGame() {
+        print(Message.RETRY_MESSAGE);
+        return toInt(checkCommandFormat(readLine())) == RETRY_COMMAND;
+    }
+
+    private String checkCommandFormat(String command) {
+        if (Validator.checkCommandFormat(command)) {
+            return command;
         }
         throw new IllegalArgumentException();
+    }
+
+    private int toInt(String command) {
+        return Integer.parseInt(command);
     }
 }
