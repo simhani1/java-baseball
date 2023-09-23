653d399c3664cc25d72d22b7909d301c3e06efbc
simhani1 <simhani1@gmail.com>
2023-09-23 21:40:44 +0900

    feat: service 메서드 구현 & 단위 테스트 작성

--- src/main/java/baseball/service/impl/CheckAnswerServiceImpl.java
+++ src/main/java/baseball/service/impl/CheckAnswerServiceImpl.java
@@ -9,6 +9,32 @@ public class CheckAnswerServiceImpl implements CheckAnswerService {
 
     @Override
     public GameResult checkAnswer(List<Integer> computer, List<Integer> inputNumber) {
-        return null;
+        GameResult gameResult = new GameResult(0, 0, false);
+        for (int i = 0; i < computer.size(); i++) {
+            gameResult.addStrikeCount(countStrike(computer.get(i), i, inputNumber));
+            gameResult.addBallCount(countBall(computer.get(i), i, inputNumber));
+        }
+        gameResult.changeCorrection();
+        return gameResult;
+    }
+
+    private int countBall(int std, int idx, List<Integer> inputNumber) {
+        int ballCount = 0;
+        for (int i = 0; i < inputNumber.size(); i++) {
+            if (idx != i && std == inputNumber.get(i)) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    private int countStrike(int std, int idx, List<Integer> inputNumber) {
+        int strikeCount = 0;
+        for (int i = 0; i < inputNumber.size(); i++) {
+            if (idx == i && std == inputNumber.get(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
     }
 }
